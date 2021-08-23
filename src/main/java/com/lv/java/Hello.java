6e3b4e777de1748325af5209e8f5ffce8af52f0a
lvxuesi <764034858@qq.com>
2021-08-23 21:39:48 +0800

    hot-fix-commit

--- src/main/java/com/lv/java/Hello.java
+++ src/main/java/com/lv/java/Hello.java
@@ -6,5 +6,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2222");
+        System.out.println("hello3333");
+        System.out.println("hello244444");
+
+
     }
 }
